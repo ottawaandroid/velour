4a1b535ca3a8c53e72b8e3a6ceacfb60f2c67e59
Don Kelly <karfai@gmail.com>
2011-05-15 17:10:48 -0400

    licensing...

--- src/ca/ottawaandroid/velour/Panels.java
+++ src/ca/ottawaandroid/velour/Panels.java
@@ -1,5 +1,29 @@
+/*
+ *  Part of velour - a collection of useful widgets
+ *
+ *  Copyright 2011 Don Kelly <karfai@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package ca.ottawaandroid.velour;
-
+/**
+  *
+  * A ViewGroup that displays it's children in a thumb-scrollable
+  * region. Inspired by a class called 'ScrollableViewGroup' found in
+  * an android project, now forgotten, and the main documents list in
+  * google docs for android.
+  *
+  */
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
