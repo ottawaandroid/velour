511e962303b4ed89d00491ea362b5d20055bc1e1
Don Kelly <karfai@gmail.com>
2011-05-15 13:16:50 -0400

    start building up the demo application

--- demo/src/ca/ottawaandroid/velourdemo/MainActivity.java
+++ demo/src/ca/ottawaandroid/velourdemo/MainActivity.java
@@ -1,15 +1,33 @@
 package ca.ottawaandroid.velourdemo;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity
+public class MainActivity extends ListActivity
 {
-    /** Called when the activity is first created. */
-    @Override
+    private static final String[] WIDGETS = {"Panels"};
+
+	@Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.main_list_item, WIDGETS));
+
+        ListView lv = getListView();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+          public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // When clicked, show a toast with the TextView text
+            Toast.makeText(
+            		getApplicationContext(), ((TextView) view).getText(),
+            		Toast.LENGTH_SHORT).show();
+          }
+        });
     }
 }
